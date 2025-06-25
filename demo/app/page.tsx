6c796e56986fd0a1e62f141ca6973681151f2e9a
Brian Beck <exogen@gmail.com>
2025-06-25 07:42:57 -0700

    Rebuild with page title

--- demo/app/page.tsx
+++ demo/app/page.tsx
@@ -1,6 +1,12 @@
+import type { Metadata } from "next";
 import cryptoRandomString from "crypto-random-string";
 import { loadRuleString } from "../../src";
 import { Translator } from "./Translator";
+
+export const metadata: Metadata = {
+  title: "Snacklish: Improved Flavor!",
+  description: "Snacklish translator.",
+};
 
 function getAutoRandomSeed() {
   return cryptoRandomString({ length: 6, type: "hex" });
